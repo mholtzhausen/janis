eedd387722b031782c2e92c8063f4649415d6a41
Mark Holtzhausen <mark.holtzhausen@superbalist.com>
2020-03-04 13:57:55 +0200

    Initial commit

---
+++ src/components/Main.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Hello janis</div>
+</template>
+
+<script>
+export default {
+	name: 'Main'
+}
+</script>
+
+<style>
+
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const { join } = require('path')
+const { VueLoaderPlugin } = require('vue-loader')
+const { HotModuleReplacementPlugin } = require('webpack')
+
+const config = {
+	mode: 'development',
+	entry: join(__dirname, 'src/app.js'),
+	output:{
+		path: join(__dirname, 'dist'),
+		filename: 'bundle.js'
+	},
+	devServer:{
+		port: 8080,
+		open: true,
+		historyApiFallback: true
+	},
+	module:{
+		rules:[
+			{
+				test: /\.js$/,
+				loader: 'babel-loader',
+				options: {
+					presets: ['@babel/preset-env']
+				}
+			},
+			{
+				test: /\.vue$/,
+				loader: 'vue-loader'
+			},
+			{
+				test: /\.css$/,
+				use: [
+					'vue-style-loader',
+					'css-loader'
+				]
+			}
+		]
+	},
+	plugins:[
+		new HotModuleReplacementPlugin(),
+		new VueLoaderPlugin(),
+		new HTMLWebpackPlugin({
+			showErrors: true,
+			cache: true,
+			title: 'Vue with Webpack Start Project',
+			template: join(__dirname, 'src/index.html')
+		})
+	]
+}
+
+module.exports = config
